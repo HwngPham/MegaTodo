ade4e152f21868a3f3f1b890c61493d29a072325
HwngPham <quanghwng22@gmail.com>
2023-05-27 15:50:37 +0700

    clients:vue - Add structure

---
+++ clients/vue/src/modules/todo/todo.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="store.increase()">count is {{ store.count }}</button>
+    </div>
+</template>
+
+<style scoped>
+</style>
+
+<script setup lang="ts">
+import {store} from '@/store'
+
+defineProps<{ msg: string }>()
+
+</script>
+
