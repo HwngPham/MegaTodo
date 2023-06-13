4ceaa6369b5f68dc2e7809479d04b88a8cd32166
HwngPham <quanghwng22@gmail.com>
2023-06-13 10:45:44 +0700

    servers:fastify - add .env and README.md

---
+++ servers/fastify-api/README.md
@@ -0,0 +1,21 @@
+Requirements
+
+- node >= 18.16
+- pnpm
+
+Prepare env
+```
+cp .env.example .env
+pnpm install
+```
+
+Start production server
+```
+pnpm build
+pnpm start
+```
+
+Start development server
+```
+pnpm dev
+```
--- servers/fastify-api/index.ts
+++ servers/fastify-api/index.ts
@@ -1,9 +1,12 @@
+import { config } from "dotenv";
 import { createServer } from "./main";
 
+config();
 const server = createServer();
-const port = Number(process.env.PORT) || 3000;
+const port = Number(process.env.PORT) ?? 3000;
+const host = process.env.HOST ?? "localhost";
 
-server.listen({ port }, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+server.listen({ port, host }, () => {
+  console.log(`Server is running on http://${host}:${port}`);
   console.log(server.printRoutes());
 });
