2f19e11770a82d2b43adfa3de6976be4b47adef7
HwngPham <quanghwng22@gmail.com>
2023-06-13 08:30:16 +0700

    servers:strawberry - reconfig uvicorn to sync mode

--- servers/strawberry-graph/main.py
+++ servers/strawberry-graph/main.py
@@ -1,7 +1,6 @@
-import asyncio
 from os import environ
 from dotenv import load_dotenv
-from uvicorn import Config, Server
+from uvicorn import run
 from strawberry.asgi import GraphQL
 from src.schema import schema
 
@@ -9,16 +8,14 @@ load_dotenv()
 app = GraphQL(schema)
 
 
-async def main():
-    config = Config(
-        "main:app",
+def main():
+    run(
+        app="main:app",
         host="0.0.0.0",
         port=int(environ.get("PORT", 8000)),
         reload=bool(environ.get("DEBUG", False)),
     )
-    server = Server(config)
-    await server.serve()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
