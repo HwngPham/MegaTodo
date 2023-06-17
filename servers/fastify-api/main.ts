20b87756ca05c12762e447bcb277b8f65ba13206
HwngPham <quanghwng22@gmail.com>
2023-06-17 09:42:40 +0700

    servers:fastify - config cors

--- servers/fastify-api/main.ts
+++ servers/fastify-api/main.ts
@@ -1,13 +1,21 @@
 import Fastify, { FastifyInstance, RouteShorthandOptions } from "fastify";
 import helmet from "@fastify/helmet";
+import cors from "@fastify/cors";
 import { apiTodos } from "./src/routes/todos";
 import { apiHealth } from "./src/routes/health";
 import { database } from "./src/configs/database";
 
 export const createServer = (configs: RouteShorthandOptions = {}) => {
   const app: FastifyInstance = Fastify({});
-  const decorators = [database, helmet, apiHealth, apiTodos];
 
-  decorators.forEach((d) => app.register(d));
+  app.register(database);
+  app.register(helmet);
+  app.register(apiHealth);
+  app.register(apiTodos);
+  app.register(cors, {
+    // TODO: turn off on prod
+    origin: true,
+  });
+
   return app;
 };
