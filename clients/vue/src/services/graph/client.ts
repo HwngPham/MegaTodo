3d7449d6eaf42baba9ffc30f47fbf0a1fdb0d7ab
HwngPham <quanghwng22@gmail.com>
2023-06-19 11:18:39 +0700

    clients:vue - config vite env

--- clients/vue/src/services/graph/client.ts
+++ clients/vue/src/services/graph/client.ts
@@ -1,6 +1,6 @@
 import { Client, cacheExchange, fetchExchange } from "@urql/vue";
 
 export const client = new Client({
-  url: process.env.GRAPHQL_URL ?? "http://127.0.0.1:4000/graphql",
+  url: import.meta.env.VITE_GRAPHQL_URL ?? "http://127.0.0.1:8000/graphql",
   exchanges: [cacheExchange, fetchExchange],
 });
