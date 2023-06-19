86fad6baddcd19a9fbe9a6d41793c2c01b37cd13
HwngPham <quanghwng22@gmail.com>
2023-06-19 16:02:50 +0700

    clients:vue - add element plus ui with dark theme

---
+++ clients/vue/src/modules/todo/components/input.vue
@@ -0,0 +1,8 @@
+<template>
+  <el-input v-model="input" placeholder="Let's do something..." />
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const input = ref("");
+</script>
--- clients/vue/src/modules/todo/todo.vue
+++ clients/vue/src/modules/todo/todo.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { Ref, ref, watch } from "vue";
+import Input from "./components/input.vue";
 // import { store } from "#/store";
 import TodoItem from "./components/item.vue";
 import { gql, useQuery } from "@urql/vue";
 
-let allTodo: Ref<any> = ref([])
+let allTodo: Ref<any> = ref([]);
 
 const fetchAllTodo = useQuery({
   query: gql`
@@ -24,6 +25,7 @@ watch(allTodo, console.log);
 </script>
 
 <template>
+  <Input />
   <li v-for="item in allTodo">
     <TodoItem :item="item" />
   </li>
