664e1c81ba8eaeda509c549920c1257cdaa6c1a6
hwng <quanghwng22@gmail.com>
2023-08-13 22:19:59 +0700

    clients:svelte - rename to svelte-ssr

---
+++ clients/svelte-ssr/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  export async function load({ fetch, params }) {
+    const res = await fetch("http://localhost:8000/todos");
+    const item = await res.json();
+
+    console.log("===item", item);
+    return { item };
+  }
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
